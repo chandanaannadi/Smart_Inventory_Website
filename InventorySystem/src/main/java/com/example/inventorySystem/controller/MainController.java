826aa08bc675d5b4efe0c861c4dd9c27c3f378c4
Jonah Nishanth <jonahnishanth@mail.com>
2024-10-06 22:37:48 -0500

    Added order history and product history functionality

--- InventorySystem/src/main/java/com/example/inventorySystem/controller/MainController.java
+++ InventorySystem/src/main/java/com/example/inventorySystem/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.inventorySystem.controller;
+\package com.example.inventorySystem.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
